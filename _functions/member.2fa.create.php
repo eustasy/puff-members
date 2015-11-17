0d1bb6cb3570f2ad11fb5bdb2ca8dd8a45c8b050
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-17 16:26:41 +0000

    Add 2FA functions

---
+++ _functions/member.2fa.create.php
@@ -0,0 +1,27 @@
+<?php
+
+function Puff_Member_2FA_Create($Username, $Connection) {
+
+	require_once $Sitewide['Puff']['Libs'].'authenticatron.php';
+
+	////	Check Member Existence
+	// For the sake of the space-time continuum,
+	// new users should not already exist.
+	$Username = Puff_Member_Sanitize_Username($Username);
+	$MemberExists = Puff_Member_Exists($Username, $Connection, true);
+	if ( !$MemberExists ) {
+		return array('error' => 'Sorry, that user doesn\'t exist, so we can\'t make a session for it.');
+	}
+
+	////	Generate all the 2FA Stuff
+	$Authenticatron = Authenticatron_New($Username);
+
+	////	Update Database
+	$Result = mysqli_query($Connection, 'UPDATE `Members` SET `2FA Secret`=\''.$Authenticatron['Secret'].'\' WHERE `Username`=\''.$Username.'\';');
+
+	unset($Authenticatron['Secret']);
+	return $Authenticatron;
+
+}
+
+
