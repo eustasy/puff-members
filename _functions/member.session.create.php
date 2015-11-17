3614d87a55e78a2541b2db24ec984441c4193b85
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-17 16:47:32 +0000

    De-duplication

--- _functions/member.session.create.php
+++ _functions/member.session.create.php
@@ -14,19 +14,10 @@ function Puff_Member_Session_Create($Username, $Connection) {
 	////	Generate a Session
 	// The Session will be a 128 character hexidecimal hash from a secure source.
 	// Will return an error if no secure source is available.
-	if ( function_exists('mcrypt_create_iv') ) {
-		$Session = mcrypt_create_iv(64);
-		$Strong = true;
-	} else if ( function_exists('openssl_random_pseudo_bytes') ) {
-		// Don't set strong here because the function does it based on the source.
-		$Session = openssl_random_pseudo_bytes(64, $Strong);
-	} else {
-		$Strong = false;
-	}
-	if ( !$Strong ) {
+	$Session = Puff_Member_SecureRandom();
+	if ( !$Session ) {
 		return array('error' => 'Error: No secure source was available for Session generation. Your password could not be secured. This is not your fault.');
 	}
-	$Session = bin2hex($Session);
 
 	////	Collision Chance
 	// 16 base
