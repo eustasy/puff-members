89a92411596505bbf051f7aa8b14e3e782fab1e7
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-18 10:03:36 +0000

    Remove debug

--- _functions/member.key.update.php
+++ _functions/member.key.update.php
@@ -7,7 +7,6 @@ function Puff_Member_Key_Update($Username, $Key, $Value, $Connection) {
 	$Value    = htmlentities($Value, ENT_QUOTES, 'UTF-8');
 
 	$OldValue = Puff_Member_Key_Value($Username, $Key, $Connection);
-	var_dump($OldValue);
 	if ( !$OldValue ) {
 		return array('error' => 'Sorry, that UserKeyValue combination doesn\'t exist.');
 	}
