7a326ca55456068c48cc7eae7d02610428d9d13a
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-20 16:14:15 +0000

    Remove defunct tests

--- test.member.disable.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once __DIR__.'/_puff/sitewide.php';
-	$Page['Type']  = 'Index';
-	$Page['Title'] = 'Puff Sys';
-	$Page['Description'] = 'Puff is Awesome';
-	require_once $Sitewide['Templates']['Header'];
-
-	require_once $Sitewide['Templates']['Footer'];
