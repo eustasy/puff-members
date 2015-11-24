9ca370d7d4a28f826389e93e11f68e4e4a29c0fc
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-24 11:35:51 +0000

    Update key.update.php

--- _functions/members/key.update.php
+++ _functions/members/key.update.php
@@ -6,7 +6,7 @@ function Puff_Member_Key_Update($Connection, $Username, $Key, $Value) {
 	$Key      = htmlentities($Key, ENT_QUOTES, 'UTF-8');
 	$Value    = htmlentities($Value, ENT_QUOTES, 'UTF-8');
 
-	$Result = mysqli_query($Connection, 'REPLACE INTO `KeyValues` SET `Value`=\''.$Value.'\' WHERE `Username`=\''.$Username.'\' AND `Key`=\''.$Key.'\';');
+	$Result = mysqli_query($Connection, 'REPLACE INTO `KeyValues` (`Username`, `Key`, `Value`) VALUES (\''.$Username.'\', \''.$Key.'\', \''.$Value.'\');');
 	return $Result;
 
 }
