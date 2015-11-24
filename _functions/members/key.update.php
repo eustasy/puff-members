41243edf3266c3386c2b6680474f84b0d8acbd10
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-24 11:26:39 +0000

    Update key.update.php

--- _functions/members/key.update.php
+++ _functions/members/key.update.php
@@ -6,12 +6,7 @@ function Puff_Member_Key_Update($Connection, $Username, $Key, $Value) {
 	$Key      = htmlentities($Key, ENT_QUOTES, 'UTF-8');
 	$Value    = htmlentities($Value, ENT_QUOTES, 'UTF-8');
 
-	$OldValue = Puff_Member_Key_Value($Connection, $Username, $Key);
-	if ( !$OldValue ) {
-		return array('error' => 'Sorry, that UserKeyValue combination doesn\'t exist.');
-	}
-
-	$Result = mysqli_query($Connection, 'UPDATE `KeyValues` SET `Value`=\''.$Value.'\' WHERE `Username`=\''.$Username.'\' AND `Key`=\''.$Key.'\';');
+	$Result = mysqli_query($Connection, 'REPLACE INTO `KeyValues` SET `Value`=\''.$Value.'\' WHERE `Username`=\''.$Username.'\' AND `Key`=\''.$Key.'\';');
 	return $Result;
 
 }
