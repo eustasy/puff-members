f86302fdc8d53fef006c1da1fb9ba560ce47cf3d
Lewis Goddard <lewisgoddard@eustasy.org>
2016-02-14 00:13:06 +0000

    Update test.member.php

--- _tests/members/test.member.php
+++ _tests/members/test.member.php
@@ -1,18 +1,18 @@
 <?php
-	require_once __DIR__.'/_puff/sitewide.php';
-	$Page['Type']  = 'Test';
+require_once __DIR__.'/../../_puff/sitewide.php';
+$Page['Type']  = 'Test';
 
-	$Connection = $Sitewide['Database']['Connection'];
-	$Username = '__AUTOTESTING__';
+$Connection = $Sitewide['Database']['Connection'];
+$Username = '__AUTOTESTING__';
 
-	echo 'Puff_Member_Create'.PHP_EOL;
-	$Result = Puff_Member_Create($Connection, $Username, 'Password1');
-	var_dump($Result);
+echo 'Puff_Member_Create'.PHP_EOL;
+$Result = Puff_Member_Create($Connection, $Username, 'Password1');
+var_dump($Result);
 
-	echo 'Puff_Member_Disable'.PHP_EOL;
-	$Result = Puff_Member_Disable($Connection, $Username);
-	var_dump($Result);
+echo 'Puff_Member_Disable'.PHP_EOL;
+$Result = Puff_Member_Disable($Connection, $Username);
+var_dump($Result);
 
-	echo 'Puff_Member_Destroy'.PHP_EOL;
-	$Result = Puff_Member_Destroy($Connection, $Username);
-	var_dump($Result);
+echo 'Puff_Member_Destroy'.PHP_EOL;
+$Result = Puff_Member_Destroy($Connection, $Username);
+var_dump($Result);
