673f7f61bfdca87daaaad6ca7c0bc98143430d85
Lewis Goddard <lewisgoddard@eustasy.org>
2016-03-02 14:28:11 +0000

    Fix initialization

--- _tests/test.initialize.php
+++ _tests/test.initialize.php
@@ -14,6 +14,31 @@ $queries[] = 'CREATE TABLE IF NOT EXISTS `Runonces` (
 	`Session` varchar(128) NOT NULL,
 	`Active` int(1) NOT NULL DEFAULT \'1\'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
+$queries[] = 'CREATE TABLE IF NOT EXISTS `KeyValues` (
+	`Username` varchar(128) NOT NULL,
+	`Key` varchar(128) NOT NULL,
+	`Value` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
+$queries[] = 'CREATE TABLE IF NOT EXISTS `Members` (
+	`Username` varchar(128) NOT NULL,
+	`Active` int(1) NOT NULL DEFAULT '1',
+	`2FA Active` int(1) NOT NULL DEFAULT '0',
+	`2FA Secret` varchar(16) NOT NULL,
+	`PassHash` varchar(32) NOT NULL,
+	`Password` varchar(128) NOT NULL,
+	`Salt` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
+$queries[] = 'CREATE TABLE IF NOT EXISTS `Sessions` (
+	`Username` varchar(128) NOT NULL,
+	`Active` int(1) NOT NULL DEFAULT '1',
+	`Session` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
+$queries[] = 'ALTER TABLE `KeyValues`
+	ADD PRIMARY KEY (`Username`,`Key`);';
+$queries[] = 'ALTER TABLE `Members`
+	ADD PRIMARY KEY (`Username`), ADD KEY `Active` (`Active`);';
+$queries[] = 'ALTER TABLE `Sessions`
+	ADD PRIMARY KEY (`Session`), ADD KEY `Username` (`Username`);';
 foreach ( $queries as $query ) {
 	$result = mysqli_query($test_db, $query);
 	echo 'Test initialized: ';
