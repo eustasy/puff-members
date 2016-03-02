c42ec4db72e1d4b9d0a555e8beb0769dad2d8651
Lewis Goddard <lewisgoddard@eustasy.org>
2016-03-02 13:14:44 +0000

    Update from All

--- _hooks/preload/puff-geo.php
+++ _hooks/preload/puff-geo.php
@@ -13,24 +13,20 @@ if (
 		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 	} else if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 		$ip = $_SERVER['HTTP_CLIENT_IP'];
-	} else {
+	} else if (!empty($_SERVER['REMOTE_ADDR'])) {
 		$ip = $_SERVER['REMOTE_ADDR'];
+	} else {
+		$ip = false;
 	}
 	// Require GeoIP2
 	require_once $Sitewide['Puff']['Libs'].'geoip2.phar';
 	// Try and do the thing
 	try {
-		if ( $debug ) {
-			echo $ip."\n";
-		}
 		if (!class_exists('GeoIp2\Database\Reader')) {
 			throw new \Exception('Class GeoIp2\Database\Reader not found');
 		}
 		$reader = new Reader($Sitewide['Puff']['Libs'].'GeoLite2-City.mmdb');
 		$record = $reader->city($ip);
-		if ( $debug > 1 ) {
-			var_dump($record);
-		}
 		$Sitewide['Geo']['Continent'] = $record->continent->code;
 		$Sitewide['Geo']['Country']   = $record->country->isoCode;
 	// Catch any failures, but be nice about it.
@@ -39,12 +35,6 @@ if (
 		$Sitewide['Geo']['Continent'] = false;
 		$Sitewide['Geo']['Country']   = false;
 	}
-
-	if ( $debug ) {
-		echo 'Continent: "'.$Sitewide['Geo']['Continent'].'"'."\n";
-		echo 'Country: "'.$Sitewide['Geo']['Country'].'"'."\n";
-	}
-
 } else {
 	$Sitewide['Geo']['Continent'] = false;
 	$Sitewide['Geo']['Country']   = false;
