15b4443a710ea4d61b4c09cf6a55c20708701b3c
Lewis Goddard <lewisgoddard@eustasy.org>
2016-03-02 14:25:45 +0000

    Fix tests and 2fa

--- _tests/members/test.member.session.php
+++ _tests/members/test.member.session.php
@@ -3,32 +3,38 @@ require_once __DIR__.'/../../_puff/sitewide.php';
 $Page['Type']  = 'Test';
 
 $Connection = $Sitewide['Database']['Connection'];
-$Username = '__AUTOTESTING__';
+$Username = '__AUTOTESTING_MEMBER_SESSION__';
 
 echo 'Puff_Member_Create'.PHP_EOL;
-$Result = Puff_Member_Create($Connection, $Username, 'Password1');
-var_dump($Result);
+$Result['Member'] = Puff_Member_Create($Connection, $Username, 'Password1');
+var_dump($Result['Member']);
 
 echo 'Puff_Member_Session_Create'.PHP_EOL;
-$Result = Puff_Member_Session_Create($Connection, $Username);
-var_dump($Result);
+$Result['Create'] = Puff_Member_Session_Create($Connection, $Username);
+var_dump($Result['Create']);
 
 echo 'Puff_Member_Session_Disable'.PHP_EOL;
-$Result = Puff_Member_Session_Disable($Connection, $Result['Session']);
-var_dump($Result);
+$Result['Disable'] = Puff_Member_Session_Disable($Connection, $Result['Create']['Session']);
+var_dump($Result['Disable']);
 
 echo 'Puff_Member_Session_Create'.PHP_EOL;
-$Result = Puff_Member_Session_Create($Connection, $Username);
-var_dump($Result);
+$Result['Create2'] = Puff_Member_Session_Create($Connection, $Username);
+var_dump($Result['Create2']);
 
 echo 'Puff_Member_Session_Create'.PHP_EOL;
-$Result = Puff_Member_Session_Create($Connection, $Username);
-var_dump($Result);
+$Result['Create3'] = Puff_Member_Session_Create($Connection, $Username);
+var_dump($Result['Create3']);
 
 echo 'Puff_Member_Session_Disable_All'.PHP_EOL;
-$Result = Puff_Member_Session_Disable_All($Connection, $Username);
-var_dump($Result);
+$Result['DisableAll'] = Puff_Member_Session_Disable_All($Connection, $Username);
+var_dump($Result['DisableAll']);
+
+// TODO Exists
 
 echo 'Puff_Member_Destroy'.PHP_EOL;
-$Result = Puff_Member_Destroy($Connection, $Username);
-var_dump($Result);
+$Result['Destroy'] = Puff_Member_Destroy($Connection, $Username);
+var_dump($Result['Destroy']);
+
+if ( in_array(false, $Result, true) ) {
+	exit(1);
+}
