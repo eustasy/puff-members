03e7563299614e77ffe4588c9a6f1d5385d62ed4
Lewis Goddard <lewisgoddard@eustasy.org>
2017-10-16 17:17:02 +0100

    Remove sessions older than a month to stop table growth

--- _cron/hourly/sessions.php
+++ _cron/hourly/sessions.php
@@ -1,6 +1,6 @@
 <?php
 
-$SQL = 'DELETE FROM `Sessions` WHERE `Active`=\'0\'';
+$SQL = 'DELETE FROM `Sessions` WHERE `Active`=\'0\' OR `Created` < NOW() - INTERVAL 1 MONTH;';
 $Result = mysqli_query($Sitewide['Database']['Connection'], $SQL);
 if ( $Result ) {
 	echo 'Success: Old Sessions were deleted.'."\n";
