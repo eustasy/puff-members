6b7e24c46077e232befd85f5a5024a3c49cbe555
Lewis Goddard <lewisgoddard@eustasy.org>
2018-05-04 21:00:10 +0100

    Update member.password.php

--- _functions/members/member.password.php
+++ _functions/members/member.password.php
@@ -11,16 +11,8 @@ function Puff_Member_Password($Connection, $Username, $Password, $CurrentSession
 		return array('error' => 'Sorry, we can\'t change the password for a member that doesn\'t exist.');
 	}
 
-	////	Re-Generate a Salt
-	// The salt will be a 128 character hexidecimal hash from a secure source.
-	// Will return an error if no secure source is available.
-	$Salt = Puff_SecureRandom();
-	if ( !$Salt ) {
-		return array('error' => 'Error: No secure source was available for Salt generation. Your password could not be secured. This is not your fault.');
-	}
-
 	////	Hash Password
-	$Hashed = Puff_Member_PassHash($Password, $Salt);
+	$Hashed = Puff_Member_PassHash($Password);
 
 	////	Disable existing Sessions
 	Puff_Member_Session_Disable_All($Connection, $Username, $CurrentSession);
