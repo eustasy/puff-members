6d1aff5689487e86bd46be9b60cd0332f3e4da51
Lewis Goddard <lewisgoddard@eustasy.org>
2018-05-04 21:00:03 +0100

    Update member.create.php

--- _functions/members/member.create.php
+++ _functions/members/member.create.php
@@ -12,16 +12,8 @@ function Puff_Member_Create($Connection, $Username, $Password) {
 		return array('error' => 'Sorry, that username is not available. Please choose a different username, or login if this is your username.');
 	}
 
-	////	Generate a Salt
-	// The salt will be a 128 character hexidecimal hash from a secure source.
-	// Will return an error if no secure source is available.
-	$Salt = Puff_SecureRandom();
-	if ( !$Salt ) {
-		return array('error' => 'Error: No secure source was available for Salt generation. Your password could not be secured. This is not your fault.');
-	}
-
 	////	Hash Password
-	$Hashed = Puff_Member_PassHash($Password, $Salt);
+	$Hashed = Puff_Member_PassHash($Password);
 
 	////	Insert into Database
 	$Result = mysqli_query($Connection, 'INSERT INTO `Members` (`Username`, `Password`, `Salt`, `PassHash`) VALUES (\''.$Username.'\', \''.$Hashed['Password'].'\', \''.$Salt.'\', \''.$Hashed['PassHash'].'\');');
